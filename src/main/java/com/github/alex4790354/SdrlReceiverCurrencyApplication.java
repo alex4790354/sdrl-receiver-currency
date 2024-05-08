662a6d341a1e4927a7f809fd15fb4382f76d33ac
Alex4790354 <9720560@gmail.com>
2024-05-08 23:28:15 +0300

    sdrl-receiver-currency. Initially commit

---
+++ src/main/java/com/github/alex4790354/SdrlReceiverCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.github.alex4790354;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdrlReceiverCurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdrlReceiverCurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alex4790354/SdrlReceiverCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alex4790354;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdrlReceiverCurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
