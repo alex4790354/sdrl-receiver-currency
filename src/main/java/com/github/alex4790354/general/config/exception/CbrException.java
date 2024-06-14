283bc5cf7a2715cd9a953107c97e0e1fba01a7c1
Alex4790354 <9720560@gmail.com>
2024-06-14 22:31:16 +0300

    Delete for review and comments

--- src/main/java/com/github/alex4790354/general/config/exception/CbrException.java
+++
@@ -1,8 +0,0 @@
-package com.github.alex4790354.general.config.exception;
-
-public class CbrException extends RuntimeException {
-
-    public CbrException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/github/alex4790354/general/config/exception/ReceiverValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.github.alex4790354.general.config.exception;
-
-public class ReceiverValidationException extends RuntimeException {
-
-    public ReceiverValidationException(String message) {
-        super(message);
-    }
-}
