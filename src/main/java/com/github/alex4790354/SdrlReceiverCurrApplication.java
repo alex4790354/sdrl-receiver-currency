ae3da850f3ff09ed9956950eef8846ca460d3e47
Alex4790354 <9720560@gmail.com>
2024-06-14 20:41:50 +0300

    spx --> sdrl

--- src/main/java/com/github/alex4790354/SdrlReceiverCurrApplication.java
+++ src/main/java/com/github/alex4790354/SdrlReceiverCurrApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SdrlReceiverCurrApplication {
 
-	// TODO: (would be better) spx => sdrl
 	public static void main(String[] args) {
 		SpringApplication.run(SdrlReceiverCurrApplication.class, args);
 	}
